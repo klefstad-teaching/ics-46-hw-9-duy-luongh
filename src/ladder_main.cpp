ed5ebde3a51a24f372ebad1a11817e9429f0ad19
Dante Luong <duy.luongh@icloud.com>
2025-03-15 23:02:41 +0000

    update ladder_main

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -9,11 +9,8 @@ int main()
     set<string> word_list;
     load_words(word_list, "../src/words.txt");
     print_word_ladder(generate_word_ladder("cat", "dog", word_list));
-    cout << endl;
     print_word_ladder(generate_word_ladder("marty", "curls", word_list));
-    cout << endl;
     print_word_ladder(generate_word_ladder("sleep", "awake", word_list));
-    cout << endl;
 
     return 0;
 }

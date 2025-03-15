4e228a31d67c5c676c12e1b34178e96add86c525
Dante Luong <duy.luongh@icloud.com>
2025-03-15 02:02:15 +0000

    update ladder_main(), add test cases

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
+    // verify shortest path
     verify_word_ladder();
+
+    // verify correct path
+    set<string> word_list;
+    load_words(word_list, "../src/words.txt");
+    print_word_ladder(generate_word_ladder("cat", "dog", word_list));
+    cout << endl;
+    print_word_ladder(generate_word_ladder("marty", "curls", word_list));
+    cout << endl;
+    print_word_ladder(generate_word_ladder("sleep", "awake", word_list));
+    cout << endl;
+
     return 0;
 }

e9e65a225a43c95033249c439adbf62ae5b67746
Dante Luong <duy.luongh@icloud.com>
2025-03-15 15:40:01 -0700

    update dijkstras_main.cpp

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
-    return 0;
+    Graph G;
+    file_to_graph("../src/small.txt", G);
+
+    vector<int> previous;
+    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+
+    for (int i = 0; i < G.numVertices; ++i)
+    {
+        vector<int> path = extract_shortest_path(distances, previous, i);
+        print_path(path, distances[i]);
+    }
 }

67a19e73ba709ef260ed3405abb317ddf8dbc0f4
Dante Luong <duy.luongh@icloud.com>
2025-03-15 01:48:09 +0000

    update path to words.txt

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -79,8 +79,6 @@ vector<string> generate_word_ladder(const string &begin_word, const string &end_
 void load_words(set<string> &word_list, const string &file_name)
 {
     ifstream in(file_name);
-    if (!in.is_open())
-        error(NULL, NULL, "Error: Could not open file ");
 
     for (string word; (in >> word);)
         word_list.insert(word);
@@ -104,7 +102,7 @@ void print_word_ladder(const vector<string> &ladder)
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 void verify_word_ladder() {
     set<string> word_list;
-    load_words(word_list, "words.txt");
+    load_words(word_list, "../src/words.txt");
     my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
     my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
     my_assert(generate_word_ladder("code", "data", word_list).size() == 6);

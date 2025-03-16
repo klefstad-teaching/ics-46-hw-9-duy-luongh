890978fdecf1cf781fd4f67cda7603bebc7ddae9
Dante Luong <duy.luongh@icloud.com>
2025-03-16 01:45:13 +0000

    change path to files

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     Graph G;
-    file_to_graph("../src/small.txt", G);
+    file_to_graph("small.txt", G);
 
     vector<int> previous;
     vector<int> distances = dijkstra_shortest_path(G, 0, previous);
